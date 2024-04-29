6f7fea3decb71b68ff9bc6f962938abe8b8c82bd
AlexNaldjan <147308554+AlexNaldjan@users.noreply.github.com>
2024-04-29 17:18:18 +0200

    Register Form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import { RegisterForm } from './Components/RegisterForm/RegisterForm';
 
 function App() {
   return (
     <>
       <h1>Hello</h1>
+      <RegisterForm />
     </>
   );
 }
---
+++ client/src/Components/RegisterForm/RegisterForm.tsx
@@ -0,0 +1,10 @@
+export const RegisterForm = () => {
+  return (
+    <form>
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      <button>Зарегистрироваться</button>
+    </form>
+  );
+};
