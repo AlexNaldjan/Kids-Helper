04426586c3d5e6dd5bbc19e930dd7dc0d3a86070
AlexNaldjan <147308554+AlexNaldjan@users.noreply.github.com>
2024-05-04 21:11:08 +0200

    Fixed bag

--- client/src/api/instance.ts
+++ client/src/api/instance.ts
@@ -1,20 +1,23 @@
-import axios, { AxiosError, AxiosRequestConfig } from 'axios';
+import axios, { AxiosError } from 'axios';
 import { store } from '../store';
 import { getAccessToken, logoutUser } from '../store/auth/actionCreators';
 import Endpoints from './endpoints';
+
+export const axiosInstance = axios.create({});
 
 const urlsSkipAuth = [
   Endpoints.AUTH.LOGIN,
   Endpoints.AUTH.REFRESH,
   Endpoints.AUTH.LOGOUT,
 ];
-axiosInstance.interceptors.request.use(async config => {
-  if (config.url && urlsSkipAuth.includes(config.url)) {
-    return config;
-  }
-  const accessToken = await store.dispatch(getAccessToken());
-  if (accessToken) {
-    const autharization = `Bearer ${accessToken}`;
+axiosInstance.interceptors.request.use(
+  async config => {
+    if (config.url && urlsSkipAuth.includes(config.url)) {
+      return config;
+    }
+    const accessToken = await store.dispatch(getAccessToken());
+    if (accessToken) {
+      const autharization = `Bearer ${accessToken}`;
       config.headers = {
         ...config.headers,
         Authorization: authorization,
@@ -26,7 +29,6 @@ axiosInstance.interceptors.request.use(async config => {
 );
 
 axiosInstance.interceptors.response.use(
-
   response => response,
   (error: AxiosError) => {
     const isLoggedIn = !!store.getState().auth.authData.accessToken;
