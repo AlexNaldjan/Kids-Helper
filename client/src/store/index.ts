15cb4e0ee0944782e097829d84b93c91361f6ce5
AlexNaldjan <147308554+AlexNaldjan@users.noreply.github.com>
2024-05-10 12:26:45 +0200

    Redux Map

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -2,10 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
 import logger from 'redux-logger';
 import authReducer from './auth/authReducer';
+import markersSlice from './map/markerSlice';
+import mapSlice from './map/mapSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    markers: markersSlice,
+    map: mapSlice,
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware().concat(
